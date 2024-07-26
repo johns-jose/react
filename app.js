8c1c0b002d75c95a6094d31c3430c992b0e72491
johns <johnsjose88@gmail.com>
2024-07-26 14:21:27 +0530

    creating nested tag and sibling tags

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const heading = React.createElement('h1',{id:'heading',xyz:'abc'},"hello world from react")
+const heading = React.createElement('div',{id:'container',class:'heading'},[React.createElement('h2',{id:"sub-heading"},'i am an h2 tag'),
+    React.createElement('p',{id:"para"},'i am a paragraph tag')
+])
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
